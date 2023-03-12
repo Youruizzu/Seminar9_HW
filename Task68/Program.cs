8305aad72a859f536f5be32484a5eefdc45c968f
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-03-12 14:46:45 +0300

    Added Task68 solution

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт наибольший общий делитель (НОД) 
+// этих чисел с помощью рекурсии.
+// M = 28; N = 7 -> 7
+
+int FindGreatestCommonDiviser(int m, int n){
+    if (n == 0){
+        return m;
+    }
+    else return FindGreatestCommonDiviser(n, m % n);
+}
+
+Console.WriteLine(FindGreatestCommonDiviser(153, 318));

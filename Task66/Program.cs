532b3954ebc2062a024ef1f890f057c612d9165e
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-03-12 14:27:52 +0300

    Added Task66 solution

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Clear();
+
+int GetNumSummBetweenTwoPoints(int pointOne, int pointTwo){
+    if (pointOne == pointTwo) {
+        return pointOne;
+    }
+    else{
+        return pointOne + GetNumSummBetweenTwoPoints(pointOne+1, pointTwo);
+    }
+}
+
+
+Console.WriteLine(GetNumSummBetweenTwoPoints(4, 8));
